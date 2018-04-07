3d73736444a37045bd3f4b189802f805188c5b8e
Dacilia <1257249648@qq.com>
2018-04-07 14:33:06 +0800

    Initial Commit

---
+++ iOSCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSCamera
+//
+//  Created by 李达志 on 2018/4/7.
+//  Copyright © 2018年 LDZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
